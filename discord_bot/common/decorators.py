30ccb6c48e2c24c5d2180a8976c6c3c215c5a1f3
Gilad Wagner <gilad@cornix.io>
2021-10-31 13:22:41 +0200

    disable wrapping static methods

--- discord_bot/common/decorators.py
+++ discord_bot/common/decorators.py
@@ -1,4 +1,5 @@
 import functools
+import inspect
 import logging
 from time import sleep
 
@@ -29,11 +30,35 @@ def handle_discord_exception(method):
     return wrapper
 
 
+def is_static_method(klass, attr, value=None):
+    """Test if a value of a class is static method.
+    example::
+        class MyClass(object):
+            @staticmethod
+            def method():
+                ...
+    :param klass: the class
+    :param attr: attribute name
+    :param value: attribute value
+    """
+    if value is None:
+        value = getattr(klass, attr)
+    assert getattr(klass, attr) == value
+
+    for cls in inspect.getmro(klass):
+        if inspect.isroutine(value):
+            if attr in cls.__dict__:
+                binded_value = cls.__dict__[attr]
+                if isinstance(binded_value, staticmethod):
+                    return True
+    return False
+
+
 def wrap_all_class_methods(decorator):
     def decorate(cls):
         for attr in cls.__dict__:
             _obj = getattr(cls, attr)
-            if callable(_obj):
+            if callable(_obj) and not is_static_method:
                 setattr(cls, attr, decorator(getattr(cls, attr)))
         return cls
     return decorate
